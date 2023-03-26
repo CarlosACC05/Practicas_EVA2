e66804786ef0f8550adb1956829010a5daaee464
CarlosACC05 <124701900+CarlosACC05@users.noreply.github.com>
2023-03-26 16:22:17 -0600

    Add files via upload

---
+++ EVA2_19_INTERFACES_FIGURAS/src/eva2_19_interfaces_figuras/Figuras.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package eva2_19_interfaces_figuras;
+
+/**
+ *
+ * @author carlo
+ */
+//No se pueden instanciar las interfaces 
+//on como un ocntrato: estoy obligado a implementar
+//solo métodos abstractos y contantes
+public interface Figuras {
+    public abstract double calcularArea();
+    double calcularPeri();
+}
